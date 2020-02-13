0a095592c7b2ceb4bb369003f97c07db0344035e
LUCAS RODRIGUES SENA <msbowelite@gmail.com>
2020-02-13 16:29:46 -0300

    Add Search Api

---
+++ Advertisement.SearchApi/Services/ISearchService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Advertisement.SearchApi.Models;
+
+namespace Advertisement.SearchApi.Services
+{
+    public interface ISearchService
+    {
+        Task<List<AdvertType>> Search(string keyword);
+    }
+}
